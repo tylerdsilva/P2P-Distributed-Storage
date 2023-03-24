cf59d66398d56aa0e2e5efea70f598da646e2912
Owais Hetavkar <owais@Owaiss-MacBook-Pro.local>
2023-03-23 23:35:03 -0700

    Started with flask

---
+++ Flask/main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, session, redirect
+import json 
+import os 
+from flask_cors import CORS 
+
+
+
+
+app = Flask(__name__)
+CORS(app)
+
+app.secret_key = 'asdoakfjnadlsfma'
+
+
+
+@app.route('/is_alive')
+def isAlive():
+    return (True, 200)
+
+@app.route('/error')
+def error():
+    message = 'Hi this is a place holder.'
+    return render_template('upload-error.html', message = message)
+
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
+
+
+
+if __name__ == '__main__':
+    app.run(port=2000, debug = True)
