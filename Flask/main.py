3057183b5de3f6daee41dceff7e9da3f197ead79
tylerdsilva <tylerdsilva1@gmail.com>
2023-03-26 21:22:58 -0700

    Started flask upload flow

--- Flask/main.py
+++ Flask/main.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, flash, render_template, request, session, redirect, url_for
 import json 
 import os 
+from werkzeug.utils import secure_filename
 from flask_cors import CORS 
 
-
+UPLOAD_FOLDER = 'temp'
 
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 CORS(app)
 
 app.secret_key = 'asdoakfjnadlsfma'
@@ -18,14 +20,32 @@ def isAlive():
     return True
 
 @app.route('/error')
-def error():
-    message = 'Hi this is a place holder.'
+def error(message):
     return render_template('upload-error.html', message = message)
 
 
 @app.route('/success')
 def success():
     return render_template('success.html')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return error('No file part')
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return error('No selected file')
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return success()
+    return render_template('upload-webpage.html')
 
 
 
